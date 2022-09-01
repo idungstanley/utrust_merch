c0c7c05933ad27c220308c81bad1e57abc94eff4
idungstanley <sundaystanley56@gmail.com>
2022-09-01 23:03:42 +0100

    testing job

--- cypress/e2e/createInvoice/testcase.cy.js
+++ cypress/e2e/createInvoice/testcase.cy.js
@@ -1,5 +1,5 @@
 describe("Create Invoice", ()=>{
     it("visit site", ()=>{
-        cy.visit(" https://merchants.sandbox-utrust.com")
+        cy.visit("https://merchants.sandbox-utrust.com")
     })
 })
